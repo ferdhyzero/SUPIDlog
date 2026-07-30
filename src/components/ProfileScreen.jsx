7f0a243199f500743d20b52e08a5f3d06559936e
ferdhyzero <ahmadferdy66@gmail.com>
2026-07-30 14:34:26 +0800

    Fix Profile screen blank issue by restoring React hooks import

--- src/components/ProfileScreen.jsx
+++ src/components/ProfileScreen.jsx
@@ -1,3 +1,4 @@
+import React, { useState, useEffect } from 'react';
 import EditProfileModal from './EditProfileModal';
 
 export default function ProfileScreen({ currentUser, onOpenLogin, onLogout, onNavigate }) {
